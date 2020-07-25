3bdb7e3e9013ecd477f09a7564f08aaff27f60b6
u-mmmmk <mkmkdi21@gmail.com>
2020-07-25 16:52:26 -0400

    implemented depth and func_eval

--- connect-4.py
+++ connect-4.py
@@ -1,14 +1,7 @@
 #!/bin/python3
 
+#minimax and alpha-beta pruning implemented to play connect 4 and always win.
 import numpy as np
-
-'''
-Notes
-exceeding maximum recursion depth
-    add a depth variable
-    add evaluate function that factors in how close the cpu is to winning
-    find way to evaluate the best moves first
-'''
 
 def main():
     print("Connect 4!")
@@ -37,6 +30,8 @@ def main():
         cpu = "[x]"
     else:
         print("Invalid Character. \nComputer will move first.")
+        player = "[O]"
+        cpu = "[X]"
     while True:
         print(board)
         cpumove()
@@ -119,61 +114,131 @@ def cpumove():
     global board
     alpha = -99999
     beta = 99999
+    depth = 8
     print("cpu move")
     bestscore = -9999
-    for i in range(6):
-        for j in range(7):
+    for i in range(5, 0 , -1):
+        for j in range(6, 0, -1):
             if checkspace(i, j) == True:
                 board[i][j] = cpu
-                score = minimax(board, alpha, beta, False)
-                board[i][j] = "[ ]" #rever the baord back to normal
+                score = minimax(depth, alpha, beta, False)
+                board[i][j] = "[ ]" #revert the baord back to normal
                 if score > bestscore:
                     bestscore = score
                     row = i
                     column = j
     board[row][column] = cpu
 
-def minimax(position, alpha, beta, maximize):
+def minimax(depth, alpha, beta, maximize):
+    global board
+    depth = depth - 1
     if checkwin() == cpu:
-        return 4
+            return 4
     elif checkwin() == player:
         return -4
     elif checktie() == True:
         return 0
+    elif depth == 0:
+        return func_eval()
+    
     if maximize == True: #if cpu turn
         maxscore = -9999
-        for i in range(6): #find all open spaces
-            for j in range(7):
+        for i in range(5, 0, -1): #find all open spaces
+            for j in range(6, 0, -1):
                 if checkspace(i, j) == True:
                     board[i][j] = cpu
-                    score = minimax(position, alpha, beta, False) #call minimax on open space
-                    position[i][j] = "[ ]" #not sure if i need to do this
+                    score = minimax(depth, alpha, beta, False) #call minimax on open space
+                    board[i][j] = "[ ]"
                     maxscore = max(maxscore, score) #find the maxevaluation
                     alpha = max(alpha, maxscore) #pruning
-                    if beta <= alpha:
-                        return alpha
+                    if alpha >= beta:
+                         return maxscore
         return maxscore
 
     else: #if players turn
         minscore = 9999
-        for i in range(6): #find all open spaces
-            for j in range(7):
+        for i in range(5, 0, -1): #find all open spaces
+            for j in range(6, 0, -1):
                 if checkspace(i, j) == True:
                     board[i][j] = player
-                    score = minimax(position, alpha, beta, True) #call minimax on open space
-                    position[i] = "[ ]"
+                    score = minimax(depth, alpha, beta, True) #call minimax on open space
+                    board[i][j] = "[ ]"
                     minscore = min(minscore, score) #set minscore to smallest score
                     beta = min(beta, minscore) #pruning
                     if beta <= alpha:
-                        return beta
+                        return minscore
         return minscore
 
-
-
-
-
-
-
+def func_eval():
+    eval = 0 #eval is based on how close the position is to winning ex 3 in a row gives 2 points
+    '''
+    #eval the position for cpu
+    I'm not sure how much this helps or if it does.
+    for i in range(6):
+        for j in range(4):
+            if board[i][j] == cpu: 
+                if board[i][j] == board[i][j + 1]:
+                    eval = eval + 1
+                    if board[i][j + 1] == board[i][j + 2]:
+                        eval = eval + 1
+    #vertical
+    for i in range(3):
+        for j in range(7):
+            if board[i][j] == cpu:
+                if board[i][j] == board[i + 1][j]:
+                    eval = eval + 1
+                    if board[i + 1][j] == board[i + 2][j]:
+                        eval = eval + 1
+    #right diagonals
+    for i in range(3):
+        for j in range(4):
+            if board[i][j] == cpu:
+                if board[i][j] == board[i + 1][j + 1]:
+                    eval = eval + 1
+                    if board[i + 1][j + 1] == board[i + 2][j + 2]:
+                        eval = eval + 1
+    #left diagonals
+    for i in range(3):
+        for j in range(3, 7):
+            if board[i][j] == cpu:
+                if board[i][j] == board[i + 1][j - 1]:
+                    eval = eval + 1
+                    if board[i + 1][j - 1] == board[i + 2][j - 2]:
+                        eval = eval + 1
+    '''
+    #penalize the position for how many pieces the player has in a row
+    for i in range(6):
+        for j in range(4):
+            if board[i][j] == player: 
+                if board[i][j] == board[i][j + 1]:
+                    eval = eval - 1
+                    if board[i][j + 1] == board[i][j + 2]:
+                        eval = eval - 1
+    #vertical
+    for i in range(3):
+        for j in range(7):
+            if board[i][j] == player:
+                if board[i][j] == board[i + 1][j]:
+                    eval = eval - 1
+                    if board[i + 1][j] == board[i + 2][j]:
+                        eval = eval - 1
+    #right diagonals
+    for i in range(3):
+        for j in range(4):
+            if board[i][j] == player:
+                if board[i][j] == board[i + 1][j + 1]:
+                    eval = eval - 1
+                    if board[i + 1][j + 1] == board[i + 2][j + 2]:
+                        eval = eval - 1
+    #left diagonals
+    for i in range(3):
+        for j in range(3, 7):
+            if board[i][j] == player:
+                if board[i][j] == board[i + 1][j - 1]:
+                    eval = eval - 1
+                    if board[i + 1][j - 1] == board[i + 2][j - 2]:
+                        eval = eval - 1
+    return eval
 
 if __name__ == '__main__':
     main() 
